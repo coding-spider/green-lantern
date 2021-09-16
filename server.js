ccb30c8a25fb7471a3ca46c517e78cc31986d539
Ro Han <19rohan92@gmail.com>
2021-09-16 23:43:27 +0530

    fix: added index page

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ const APP_NAME = 'Green Lantern'
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  return res.send('Green Lantern is happy to serve!');
+})
+
 // Routes
 app.use("/api/case-study", require("./controllers/case-study-ctrl.js"));
 
